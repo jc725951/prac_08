d8494f877fb5735f4d3b85ae054e7ebb2cd6977c
Aaishwinder kaur <aaishwinderkaur@my.jcu.edu.au>
2020-05-25 06:32:19 +0530

    "test silver_service_taxi"

---
+++ silver_service_taxi_test.py
@@ -0,0 +1,24 @@
+
+
+"""
+SilverServiceTaxi class tests
+
+"""
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+
+    """Test SilverServiceTaxi."""
+
+    taxi = SilverServiceTaxi("Test Fancy Taxi", 100, 2)
+
+    taxi.drive(18)
+
+    print(taxi)
+
+    print(taxi.get_fare())
+
+
+
+main()
