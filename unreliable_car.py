fbe6322167c4151316d762004c05c74935ed8cab
Aaishwinder kaur <aaishwinderkaur@my.jcu.edu.au>
2020-05-25 06:16:24 +0530

    "Added unreliable_car.py"

---
+++ unreliable_car.py
@@ -0,0 +1,39 @@
+from random import randint
+
+from car import Car
+
+
+
+
+
+class UnreliableCar(Car):
+
+    """An unreliable version of a car."""
+
+
+
+    def __init__(self, name, fuel, reliability):
+
+        """begin with a UnreliableCar."""
+
+        super().__init__(name, fuel)
+
+        self.reliability = reliability
+
+
+
+    def drive(self, distance):
+
+        """Drive class for drive the car based on thw reliability."""
+
+        random_number = randint(1, 100)
+
+        if random_number >= self.reliability:
+
+            distance = 0
+
+
+
+        distance_driven = super().drive(distance)
+
+        return distance_driven
