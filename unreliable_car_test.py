66a7ddfdec7e7b2ae73ed5f27c34e9cd271b1acc
Aaishwinder kaur <aaishwinderkaur@my.jcu.edu.au>
2020-05-25 06:14:51 +0530

    "test unreliable car"

---
+++ unreliable_car_test.py
@@ -0,0 +1,41 @@
+"""Unreliablecar class test"""
+
+
+from unreliable_car import UnreliableCar
+
+
+
+
+
+def main():
+
+    """Test some UnreliableCars."""
+
+
+
+    # cars with different reliabilities
+
+    good_car = UnreliableCar("Mostly Good", 100, 90)
+
+    bad_car = UnreliableCar("Dodgy", 100, 9)
+
+
+
+    # attempt to drive the cars many times
+
+    # output based on the driven distance
+
+    for i in range(1, 12):
+
+        print("Attempting to drive {}km:".format(i))
+
+        print("{:12} drove {:2}km".format(good_car.name, good_car.drive(i)))
+
+        print("{:12} drove {:2}km".format(bad_car.name, bad_car.drive(i)))
+
+
+    print(good_car)
+
+    print(bad_car)
+
+main()
